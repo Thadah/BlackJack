d0069ffe37dea92f3f5c5c0192dd16de7de52572
Iker <ikerzalla@gmail.com>
2015-04-07 18:59:42 +0200

    BlackJack klasearen metodoen buruak eginda eta Klase diagraman BlackJack klasearen metodo guztiak (partidaJolastu() izan ezik) private jarri ditut.

--- src/org/pmoo/blackjack/BlackJack.java
+++ src/org/pmoo/blackjack/BlackJack.java
@@ -1,5 +1,42 @@
 package org.pmoo.blackjack;
 
 public class BlackJack {
+	
+	private static BlackJack helbidea = null;
+	private int apostuMax;
+	private int botea;
+	private int txanda;
 
+	private BlackJack(){}
+	
+	public static synchronized BlackJack getNireBlackJack(){
+		if(helbidea == null){
+			helbidea = new BlackJack();
+		}
+		return helbidea;
+	}
+	
+	public void partidaJolastu(){
+		//TODO
+	}
+	
+	private void irabazleaKalkulatu(){
+		//TODO
+	}
+	
+	private void partidaAmaitu(){
+		//TODO
+	}
+	
+	private void apostuaEgin(){
+		//TODO
+	}
+	
+	private void kartakJolastu(){
+		//TODO
+	}
+	
+	
+	//TODO
 }
+
