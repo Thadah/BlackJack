3dafcef438f9ec651753109c7e68d2f68a8106e2
Mielotxim <ekaneiza8@gmail.com>
2015-04-07 18:25:24 +0200

    ListaJokalariak inplementatuta

--- src/org/pmoo/blackjack/ListaJokalariak.java
+++ src/org/pmoo/blackjack/ListaJokalariak.java
@@ -16,5 +16,39 @@ public class ListaJokalariak {
 		
 	}
 	
+	public Iterator<Jokalaria> getIteradorea(){
+		return this.lista.iterator();
+	}
 	
+	public Jokalaria eskuHandienaKalkulatu(){
+		Jokalaria jok,eskuHandienaDuenJOkalaria=null;
+		Iterator<Jokalaria> itr=this.getIteradorea();
+		int eskua=0;
+		while(itr.hasNext()){
+			jok=itr.next();
+			if(eskua<jok.eskuaKalkulatu()){
+				eskua=jok.eskuaKalkulatu();
+				eskuHandienaDuenJOkalaria=jok;
+			}
+		}
+		return eskuHandienaDuenJOkalaria;
+	}
+	
+	public int tamaina(){
+		return this.lista.size();
+	}
+	
+	public void ezabatuJok(Jokalaria pJok){
+		this.lista.remove(pJok);
+	}
+	
+	public void erreseteatu(){
+		this.lista.clear();
+		ListaJokalariak.helbidea=null;
+	}
+	
+	public void guztiakErreseteatu(){
+		Baraja.getBaraja().erreseteatu();
+		ListaJokalariak.getNireListaJokalariak().erreseteatu();
+	}
 }
