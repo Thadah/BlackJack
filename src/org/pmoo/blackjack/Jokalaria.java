a696e0e157dd316f9997e55bf956574c73ecc1d9
Iker <ikerzalla@gmail.com>
2015-04-07 17:39:42 +0200

    Jokalaria eta ListaKartak klaseak hasi ditut

--- src/org/pmoo/blackjack/Jokalaria.java
+++ src/org/pmoo/blackjack/Jokalaria.java
@@ -1,5 +1,40 @@
 package org.pmoo.blackjack;
 
+import java.util.*;
+
 public class Jokalaria {
+	
+	private ListaKartak eskua;
+	private String izena;
+	private int dirua;
+	private int apostua;
+	
+	public Jokalaria(String pIzena, int pDirua, int pApostua){
+		this.eskua = new ListaKartak();
+		this.izena = pIzena;
+		this.dirua = pDirua;
+		this.apostua = pApostua;
+	}
+	
+	private void apostuaEgin(){
+		//TODO
+	}
+	
+	public int eskuaKalkulatu(){//Metodo honek bakarik deitu beharko lioke listaKartak klaseko kartenBalioaLortu() metodoari
+		return this.eskua.kartenBalioaLortu();
+	}
+	
+	public void kartaEskatu(){
+		//TODO
+	}
+	
+	public void txanda(){
+		//TODO
+	}
+	
+	public int getApostua(){
+		return this.apostua;
+	}
+	
 
 }
--- src/org/pmoo/blackjack/ListaKartak.java
+++ src/org/pmoo/blackjack/ListaKartak.java
@@ -1,5 +1,36 @@
 package org.pmoo.blackjack;
 
+import java.util.*;
+
 public class ListaKartak {
+	
+	private ArrayList<Karta> kartak;
+	
+	public ListaKartak(){
+		this.kartak = new ArrayList<Karta>();
+	}
+	
+	public void gehituKarta(Karta pKarta){
+		this.kartak.add(pKarta);
+	}
+	
+	public void erreseteatu(){
+		this.kartak = null;
+	}
+	
+	
+	private Iterator<Karta> getIteradorea(){
+		return this.kartak.iterator();
+	}
+	
+	public int kartenBalioaLortu(){//Gero agian metodo hau aldatu beharko da batekoa dela-eta
+		Karta kartaBat;
+		int balioa = 0;
+		Iterator<Karta> itr=this.getIteradorea();
+		while(itr.hasNext()){
+			kartaBat = 
+			balioa = balioa + kartaBat.getKartaBalioa;
+		}
+	}
 
 }
