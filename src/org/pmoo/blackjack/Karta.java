4e0c2c06f282de73587cbef97a07ca3bfbf61e2f
Mielotxim <ekaneiza8@gmail.com>
2015-04-07 17:03:03 +0200

    karta inplementatuta eta listaJOkalariak inplentatu erdia

--- src/org/pmoo/blackjack/Karta.java
+++ src/org/pmoo/blackjack/Karta.java
@@ -1,5 +1,22 @@
 package org.pmoo.blackjack;
 
 public class Karta {
-
+		private int zenbakia;
+		private int palua;
+		
+		public Karta(int pZenbakia, int pPalua){
+			
+			this.palua=pPalua; 
+			this.zenbakia=pZenbakia;
+			
+		}
+		
+		public void idatziKarta(){
+			System.out.println("zure kartaren balioa hau da:"+this.zenbakia);
+			System.out.println("zure kartaren palua hau da:"+this.palua);
+		}
+		
+		public int getKartaBalioa(){
+			return this.zenbakia;
+		}
 }
--- src/org/pmoo/blackjack/ListaJokalariak.java
+++ src/org/pmoo/blackjack/ListaJokalariak.java
@@ -1,5 +1,20 @@
 package org.pmoo.blackjack;
-
+import java.util.*;
 public class ListaJokalariak {
-
+	private static ListaJokalariak helbidea=null;
+	private ArrayList<Jokalaria> lista;
+	
+	private ListaJokalariak(){
+		this.lista=new ArrayList<Jokalaria>();
+	}
+	
+	public static synchronized ListaJokalariak getNireListaJokalariak(){
+		if (ListaJokalariak.helbidea==null){
+			ListaJokalariak.helbidea=new ListaJokalariak();
+		}
+		return ListaJokalariak.helbidea;
+		
+	}
+	
+	
 }
