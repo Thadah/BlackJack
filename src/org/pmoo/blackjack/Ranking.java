26a65e02a75083bcd161753e7dc5c2235acae717
Xabier Lekunberri <xlekunberri@gmail.com>
2015-04-23 17:28:31 +0200

    Ranking klasearen metodo batzuk

--- src/org/pmoo/blackjack/Ranking.java
+++ src/org/pmoo/blackjack/Ranking.java
@@ -1,11 +1,41 @@
 package org.pmoo.blackjack;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Ranking {
+
+	//Atributuak
 	private ArrayList<Jokalaria> ranking;
 	
+	//Eraikitzailea
 	public Ranking(){
 		this.ranking = new ArrayList<Jokalaria>();
 	}
+	
+	//Beste Metodoak
+	private Iterator<Jokalaria> getIteradorea(){
+		return this.ranking.iterator();
+	}
+	
+	public void rankingeanSartu (Jokalaria pJokalaria){
+		this.ranking.add(pJokalaria);
+	}
+	
+	public int irabazleKop(){
+		return this.ranking.size();
+	}
+	
+	public void boteaBanatu(){
+		BlackJack mahaia = BlackJack.getNireBlackJack();
+		int bakoitzari = mahaia.getBotea()/this.irabazleKop();
+		Iterator<Jokalaria> itr = this.getIteradorea();
+		Jokalaria jokalariBat;
+		while(itr.hasNext()){
+			jokalariBat = itr.next();
+			jokalariBat.boteaHartuCroupier(bakoitzari);
+		}
+	}
+		
 }
+
