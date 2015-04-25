2e2e5f268ede2787d7eabdd5cf901a562c83352a
Xabier Lekunberri <xlekunberri@gmail.com>
2015-04-25 16:32:47 +0200

    Update ListaPartidak.java

--- src/org/pmoo/blackjack/ListaPartidak.java
+++ src/org/pmoo/blackjack/ListaPartidak.java
@@ -1,11 +1,14 @@
 package org.pmoo.blackjack;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ListaPartidak {
+	
+	//Atributuak
 	private static ListaPartidak helbidea;
 	private ArrayList<Ranking> listaPartidak;
 	
+	//Eraikitzailea
 	private ListaPartidak(){
 		this.listaPartidak = new ArrayList<Ranking>();
 	}
@@ -15,5 +18,26 @@ public class ListaPartidak {
 			ListaPartidak.helbidea = new ListaPartidak();
 		}
 		return ListaPartidak.helbidea;
+	}
+	
+	//Beste Metodoak
+	private Iterator<Ranking> getIteradorea(){
+		return this.listaPartidak.iterator();
+	}
+	
+	public void partidaGorde(Ranking pRanking){
+		this.listaPartidak.add(pRanking);
+	}
+	
+	public void partidakIdatzi(){
+		Iterator<Ranking> itr = this.getIteradorea();
+		Ranking rankingBat = null;
+		int i = 1;
+		while(itr.hasNext()){
+			rankingBat = itr.next();
+			System.out.println(i + ". Partida:");
+			rankingBat.partidakIdatzi();
+			i++;
+		}		
 	}
 }
